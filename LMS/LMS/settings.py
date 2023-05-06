01d0f296b9e97649d7fe50b5583593f2b53a370b
utsav-04 <utsavraj.ptn04@gmail.com>
2023-05-07 01:19:21 +0530

    few Changes

--- LMS/LMS/settings.py
+++ LMS/LMS/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'LMS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
